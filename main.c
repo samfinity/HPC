aeeb04eed800278c4a679a70ec4cfcb9124b8c7d
samfinity <samsonpetros@outlook.com>
2023-01-10 21:46:00 +0100

    test proc

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <mpi.h>
 
-int main(){
 
-    printf("hello world samson");
-    return 0;
+int main(int argc, char **argv){
+
+   int nbp, rank;
+
+   MPI_Init(&argc, &argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &nbp);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
+   printf("I am the pro %d among %d\n", rank,nbp);
+
+    MPI_Finalize();
 }
