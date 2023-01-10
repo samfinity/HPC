552b197ac0f8aee04c4d0d4de4634f3074fd8a84
samfinity <samsonpetros@outlook.com>
2023-01-10 21:54:09 +0100

    evenodd

--- main.c
+++ main.c
@@ -10,8 +10,15 @@ int main(int argc, char **argv){
    MPI_Comm_size(MPI_COMM_WORLD, &nbp);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    if(rank%2 == 0){
 
-   printf("I am the pro %d among %d\n", rank,nbp);
+    printf("I am the pro %d, my rank is even", rank);
+
+    }else{
+        printf("I am the pro %d, my rank is odd", rank);
+    }
+
+
 
     MPI_Finalize();
 }
