3ee1f1042d9a240fd08f52603a61977aef5df0c0
samfinity <samsonpetros@outlook.com>
2023-01-11 00:13:15 +0100

    pinpong1

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 #include <mpi.h>
 
 
-int main(int argc, char **argv){
+//int main(int argc, char **argv){
 
-   int nbp, rank;
+   //int nbp, rank;
    //int value;
    //MPI_Status stat_msg;
 
 
-
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &nbp);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//
+   //MPI_Init(&argc, &argv);
+   //MPI_Comm_size(MPI_COMM_WORLD, &nbp);
+   //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 /*
     //point to point with combi de send recv
    if(rank==4){
@@ -25,7 +25,7 @@ int main(int argc, char **argv){
 
 
     //simul send and recv avec MPI_Sendrecv
-
+/*
     int msg,value;
 
 
@@ -37,4 +37,43 @@ int main(int argc, char **argv){
 printf("I, process %d, I received %d from process %d.\n", rank,value,num_proc);
 
     MPI_Finalize();
-}
+
+    */
+
+int main(int argc, char *argv[])
+{
+int my_rank, nb_procs;
+MPI_Status status;
+char s;
+char r[20];
+
+ MPI_Init(&argc, &argv);
+ MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+ MPI_Comm_size(MPI_COMM_WORLD, &nb_procs);
+
+ switch (my_rank){
+ case 0 : s='h'; break ;
+ case 1 : s='e'; break ;
+ case 2 : s='l'; break ;
+ case 3 : s='l'; break ;
+ case 4 : s='o'; break ;
+ }
+
+ MPI_Send(&s, 1, MPI_CHAR, 0 /* destinataire */, 99 /* tag */, MPI_COMM_WORLD);
+
+ if (my_rank == 0) {
+ for (int i=0; i<nb_procs; i++){
+ MPI_Recv(r+i, 1, MPI_CHAR, MPI_ANY_SOURCE /* emetteur */,
+ 99 /* tag */, MPI_COMM_WORLD, &status);
+ }
+ r[nb_procs] = 0;
+ printf("%d a recu : %s\n", my_rank, r);
+ }
+
+ MPI_Finalize();
+ return 0;
+ }
+
+
+
+//}
